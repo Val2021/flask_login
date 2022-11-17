741f62c14d7372504dae8e13b7dcad7c9af50fb2
Val Araujo <val.spminas@gmail.com>
2022-11-17 20:41:22 -0300

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+login_manager = LoginManager(app)
+db = SQLAlchemy(app)
