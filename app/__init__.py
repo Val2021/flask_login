69daa062b20bad2de9f75bd4358798157f8cd1f2
Val Araujo <val.spminas@gmail.com>
2022-11-17 21:56:40 -0300

    feat:login and register route have been  added

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
-login_manager = LoginManager(app)
+app = Flask(__name__, template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://flaskpostgres:flask2022@postgres:5432/flaskdb'
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 db = SQLAlchemy(app)
+
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app import app
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+app.run(debug=True)
+
